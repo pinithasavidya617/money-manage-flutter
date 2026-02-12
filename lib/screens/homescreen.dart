7c44db86d61ac43821752ec4ccc40b5bbd497e2d
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-02-12 12:53:47 +0530

    Add text fields

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -15,25 +15,58 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
       ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.only(top: 20),
-          child: Column(
+      body: Column(
+        children: [
+          Center(
+            child: Container(
+              padding: EdgeInsets.only(top: 20),
+              child: Column(
+                children: [
+                  Container(
+                    height: 300,
+                    width: 300,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xFFEDECF5)
+                    ),
+                    child: Center(
+                        child: Image.asset('assets/images/wallet.jpeg', width: 200,)
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Column(
             children: [
               Container(
-                height: 300,
-                width: 300,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Color(0xFFEDECF5)
-                ),
-                child: Center(
-                  child: Image.asset('assets/images/wallet.jpeg', width: 200,)
-                ),
-              )
+                padding: EdgeInsets.only(top: 30),
+                child: Text("Save your money",
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.grey,
+                  fontStyle: FontStyle.normal),),
+              ),
+              Container(
+                child: Text("with Expense Tracker",
+                  style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold, color: Colors.grey,
+                      fontStyle: FontStyle.normal),),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 28),
+                child: Text(
+                    "Save money! The more your money\n"
+                    "works for you, the less you have to work\n"
+                    "for money",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18, color: Colors.grey, height: 1.5)),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+
             ],
-          ),
-      )
-    ),);
+            
+          )
+        ],
+      ),);
   }
 }
